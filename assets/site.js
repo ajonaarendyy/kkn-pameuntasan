22162969d7d2966d59b416ea70c389a83cde2a9a
ajonarendyy <fadlianafrendyy@gmail.com>
2026-08-18 09:30:32 +0700

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -5,7 +5,7 @@ document.documentElement.classList.add("js");
  * Nomor WhatsApp memakai format internasional tanpa tanda +, contoh: 6281234567890.
  */
 const SITE_CONFIG = {
-  whatsappNumber: "",
+  whatsappNumber: "6285156232413",
   instagramUrl: "https://www.instagram.com/desa_pameuntasan/",
   tiktokUrl: "https://www.tiktok.com/@kknm.unla.pameuntasan",
 };
